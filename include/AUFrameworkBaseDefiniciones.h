bcb153fc31056a8fc19366d31ec310c08d474ddd
Marcos Ortega <marcosjom@hotmail.com>
2025-07-04 15:12:59 +0100

    Using nbframework Mutex and Conditionals

--- include/AUFrameworkBaseDefiniciones.h
+++ include/AUFrameworkBaseDefiniciones.h
@@ -22,6 +22,10 @@
 #define NB_PRIORIDAD_CONSTRUCTOR_BASE			101
 #define NB_PRIORIDAD_CONSTRUCTOR_REGISTRADOR	102
 
+#include "nb/core/NBThread.h"
+#include "nb/core/NBThreadMutex.h"
+#include "nb/core/NBThreadCond.h"
+
 /*********************************************************/
 /*** DEFINICIONES Y TIPOS DE DATOS USADOS EN EL CODIGO ***/
 /*********************************************************/
@@ -31,32 +35,22 @@
 //#define AU_ESCALA_PIXELES_POR_METRO		64.0f
 
 #if defined(WIN32) || defined(_WIN32)
-	#define NBHILO_SLEEP_MS(MS)						Sleep(MS);
 	#define NBHILO_CLASE							HANDLE
-	#define NBHILO_MUTEX_CLASE						CRITICAL_SECTION
-	#define NBHILO_MUTEX_INICIALIZAR(MUTEX)			InitializeCriticalSection(MUTEX);
-	#define NBHILO_MUTEX_FINALIZAR(MUTEX)			DeleteCriticalSection(MUTEX);
-	#define NBHILO_MUTEX_ACTIVAR(mutex)				EnterCriticalSection(mutex);
-	#define NBHILO_MUTEX_DESACTIVAR(mutex)			LeaveCriticalSection(mutex);
-	#define NBHILO_COND_CLASE						CONDITION_VARIABLE
-	#define NBHILO_COND_INICIALIZAR(COND)			InitializeConditionVariable(COND);
-	#define NBHILO_COND_FINALIZAR(COND)				DeleteConditionVariable(COND);
-	#define NBHILO_COND_ESPERAR(cond, mutex)		SleepConditionVariableCS(cond, mutex, INFINITE);
-	#define NBHILO_COND_DISPARAR(cond)				WakeConditionVariable(cond);
 #else
-	#define NBHILO_SLEEP_MS(MS)						usleep(MS * 1000);
 	#define NBHILO_CLASE							pthread_t
-	#define NBHILO_MUTEX_CLASE						pthread_mutex_t
-	#define NBHILO_MUTEX_INICIALIZAR(MUTEX)			pthread_mutex_init(MUTEX, NULL);
-	#define NBHILO_MUTEX_FINALIZAR(MUTEX)			pthread_mutex_destroy(MUTEX);
-	#define NBHILO_MUTEX_ACTIVAR(mutex)				pthread_mutex_lock(mutex);
-	#define NBHILO_MUTEX_DESACTIVAR(mutex)			pthread_mutex_unlock(mutex);
-	#define NBHILO_COND_CLASE						pthread_cond_t
-	#define NBHILO_COND_INICIALIZAR(COND)			pthread_cond_init(COND, NULL);
-	#define NBHILO_COND_FINALIZAR(COND)				pthread_cond_destroy(COND);
-	#define NBHILO_COND_ESPERAR(cond, mutex)		pthread_cond_wait(cond, mutex);
-	#define NBHILO_COND_DISPARAR(cond)				pthread_cond_signal(cond);
 #endif
+//
+#define NBHILO_SLEEP_MS(MS)                         NBThread_mSleep(MS);
+#define NBHILO_MUTEX_CLASE                          STNBThreadMutex
+#define NBHILO_MUTEX_INICIALIZAR(MUTEX)             NBThreadMutex_init(MUTEX);
+#define NBHILO_MUTEX_FINALIZAR(MUTEX)               NBThreadMutex_release(MUTEX);
+#define NBHILO_MUTEX_ACTIVAR(mutex)                 NBThreadMutex_lock(mutex);
+#define NBHILO_MUTEX_DESACTIVAR(mutex)              NBThreadMutex_unlock(mutex);
+#define NBHILO_COND_CLASE                           STNBThreadCond
+#define NBHILO_COND_INICIALIZAR(COND)               NBThreadCond_init(COND);
+#define NBHILO_COND_FINALIZAR(COND)                 NBThreadCond_release(COND);
+#define NBHILO_COND_ESPERAR(cond, mutex)            NBThreadCond_wait(cond, mutex);
+#define NBHILO_COND_DISPARAR(cond)                  NBThreadCond_signal(cond);
 
 #ifdef CONFIG_NB_INCLUIR_MEDICIONES_CICLOS_CPU
 	#ifdef _WIN32
